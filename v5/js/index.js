7f925791fe2851d4685023062aabe01deaf04842
sharklatan <sharklatan@gmail.com>
2019-07-21 22:37:06 -0500

    betas

---
+++ v5/js/index.js
@@ -0,0 +1,193 @@
+var appData;
+$(function() {
+    loadSearch();
+
+});
+function init(data){
+
+    var str=""
+    for (var x = 0;x <data.AllApps.length;x++){
+        str+=addRow(data.AllApps[x].id,data.AllApps[x].name,data.AllApps[x].developer,data.AllApps[x].subtitle);
+    }
+    document.getElementById("apps").innerHTML=str;
+    var str=""
+    for (var x = 0;x <data.TopApps.length;x++){
+        if(x == 10)
+            break;
+        str+=addRow(data.TopApps[x].id,data.TopApps[x].name,data.TopApps[x].developer,data.TopApps[x].subtitle);
+
+    }
+    document.getElementById("TopApps").innerHTML=str;
+    var str=""
+    for (var x = 0;x <data.TweakedApps.length;x++){
+        if(x == 5)
+            break;
+        str+=addRow(data.TweakedApps[x].id,data.TweakedApps[x].name,data.TweakedApps[x].developer,data.TweakedApps[x].subtitle);
+
+    }
+    document.getElementById("TweakedApps").innerHTML=str;
+    var str=""
+    for (var x = 0;x <data.PaidAndOtherApps.length;x++){
+        if(x == 5)
+            break;
+        str+=addRow(data.PaidAndOtherApps[x].id,data.PaidAndOtherApps[x].name,data.PaidAndOtherApps[x].developer,data.PaidAndOtherApps[x].subtitle);
+    }
+    document.getElementById("PaidAndOtherApps").innerHTML=str;
+    var str=""
+    for (var x = 0;x <data.TweakedGames.length;x++){
+        if(x == 5)
+            break;
+        str+=addRow(data.TweakedGames[x].id,data.TweakedGames[x].name,data.TweakedGames[x].developer,data.TweakedGames[x].subtitle);
+    }
+    document.getElementById("TweakedGames").innerHTML=str;
+    var str=""
+    for (var x = 0;x <data.PaidAndOtherGames.length;x++){
+        if(x == 5)
+            break;
+        str+=addRow(data.PaidAndOtherGames[x].id,data.PaidAndOtherGames[x].name,data.PaidAndOtherGames[x].developer,data.PaidAndOtherGames[x].subtitle);
+
+    }
+    document.getElementById("PaidAndOtherGames").innerHTML=str;
+    var str=""
+    for (var x = 0;x <data.RecommendedThisWeek.length;x++){
+        if(x == 5)
+            break;
+        str+=addRow(data.RecommendedThisWeek[x].id,data.RecommendedThisWeek[x].name,data.RecommendedThisWeek[x].developer,data.RecommendedThisWeek[x].subtitle);
+
+    }
+    document.getElementById("RecommendedThisWeek").innerHTML=str;
+    var str=""
+    for (var x = 0;x <data.MoviesOnIOS.length;x++){
+        if(x == 5)
+            break;
+        str+=addRow(data.MoviesOnIOS[x].id,data.MoviesOnIOS[x].name,data.MoviesOnIOS[x].developer,data.MoviesOnIOS[x].subtitle);
+
+    }
+    document.getElementById("MoviesOnIOS").innerHTML=str;
+}
+/*
+# id, name, quantity, tab
+'1', 'Tweaked Apps', '12', 'app'
+'2', 'Paid and Other Apps', '12', 'app'
+'3', 'Tweaked Games', '12', 'game'
+'4', 'Paid and Other Games', '12', 'game'
+'5', 'Featured', '12', 'today'
+'6', 'Tweaked ++ Apps', '12', 'today'
+'27', 'Recommended This Week', '12', 'today'
+'29', 'Jailbreak', '12', 'today'
+'30', 'Stream Live TV', '12', 'today'
+'31', 'Movies on iOS', '12', 'today'
+*/
+/*
+    private int id;
+    private String name;
+    private String developer;
+    private String subtitle;
+
+ */
+function loadSearch() {
+    var server ="https://topstore.vip/GetBrief/AllApps.action"
+    $.ajax({
+        type: 'GET',
+        url: server,
+        async: true,
+        dataType: 'jsonp',
+        jsonp: "jsonpCallback",
+        success: function (data) {
+            appData=data
+            init(data)
+            loadLatest()
+        },
+        error: function () {
+            loadLatest()
+        }
+    });
+}
+function loadLatest() {
+    var server ="https://topstore.vip/GetBrief/Latest.action"
+    $.ajax({
+        type: 'GET',
+        url: server,
+        async: true,
+        dataType: 'jsonp',
+        jsonp: "jsonpCallback",
+        success: function (data) {
+
+            var str=""
+            for (var x = 0;x <data.Latest.length;x++){
+                str+=addRow(data.Latest[x].id,data.Latest[x].name,data.Latest[x].developer,data.Latest[x].subtitle);
+            }
+            document.getElementById("Latest").innerHTML=str;
+
+        },
+        error: function () {
+            alert("network error");
+        }
+    });
+
+}
+function load(category){
+    var server ="https://topstore.vip/GetBrief/AppListByCategoryLimit.action?category="
+        $.ajax({
+            type: 'GET',
+            url: server+category,
+            async: true,
+            dataType: 'jsonp',
+            jsonp: "jsonpCallback",
+            success: function (data) {
+                var str=""
+                for (var x = 0;x <data.AppList.length;x++){
+                    str+=addRow(data.AppList[x].id,data.AppList[x].name,data.AppList[x].developer,data.AppList[x].subtitle);
+
+                }
+            },
+            error: function () {
+                alert("network error");
+            }
+        });
+
+
+
+    /*$.post(server+""+category,{},function(obj){
+        for (var x = 0;x <obj.AppList.length;x++){
+            str+=addRow(obj.AppList[x].id,obj.AppList[x].name,obj.AppList[x].developer,obj.AppList[x].subtitle);
+        }
+        $(getDiv(category)).append(str);
+    },"json");*/
+}
+function addRow(id,name,developer,subtitle){
+     var html = "";
+    var str="                                    <li class=\"item-content\">\n" +
+        "                                        <div class=\"item-media\">\n" +
+        "                                            <img class=\"lazy lazy-fadeIn\" src=\"https://topstore.vip/image/loading.gif\" data-src=\"https://topstore.vip/image/"+id+".png\" width=\"44\">\n" +
+        "                                        </div>\n" +
+        "                                        <div class=\"item-inner\">\n" +
+        "                                            <div class=\"item-title-row\">\n" +
+        "                                                <div class=\"app-title\">"+name+"</div>\n" +
+        "                                                <div class=\"\" style=\"margin-right: 20px;\">\n" +
+        "                                                    <a href=\"detail.html?id="+id+"\" class=\"button button-fill button-round\"style=\"background: #F0F1F6; color: #007AFF; font-weight:bold;\">GET</a>\n" +
+        "                                                </div>\n" +
+        "                                            </div>\n" +
+        "                                            <div class=\"app-subtitle\">"+subtitle+"</div>\n" +
+        "                                        </div>\n" +
+        "                                    </li>"
+    return str;
+}
+
+function getDiv(category) {
+    if(category=="Top Apps")
+        return "TopApps";
+    else if(category=="Paid and Other Apps")
+        return "PaidAndOtherApps"
+    else if(category=="Tweaked Apps")
+        return "TweakedApps"
+    else if(category=="Tweaked Games")
+        return "TweakedGames"
+    else if(category=="Paid and Other Games")
+        return "PaidAndOtherGames"
+    else if(category=="Recommended This Week")
+        return "Recommended"
+    else if(category=="Movies on iOS")
+        return "MoviesOnIOS"
+
+}
---
+++ v5/js/my-app.js
@@ -0,0 +1,189 @@
+var myApp = new Framework7({
+    modalTitle: 'SharkApp'
+});
+myApp.confirm('Follow our Twitter to get more information', function() {
+    window.location.href = "https://twitter.com/sharklatan"
+});
+var mySwiper = new Swiper('.swiper-container', {
+        preloadImages: false,
+        lazyLoading: true,
+        pagination: '.swiper-pagination'
+    })
+    // 为便于使用，自定义DOM库名字为$$
+var $$ = Dom7;
+var mySwiper = new Swiper('.swiper-container', {
+        preloadImages: true,
+        lazyLoading: true,
+        pagination: '.swiper-pagination'
+    })
+    // 添加视图
+var view1 = myApp.addView('#view-1', {
+    // 让这个视图支持动态导航栏
+    dynamicNavbar: true
+});
+var view2 = myApp.addView('#view-2', {
+    // 让这个视图支持动态导航栏
+    dynamicNavbar: true
+});
+var view3 = myApp.addView('#view-3', {
+    // 让这个视图支持动态导航栏
+    dynamicNavbar: true
+});
+var view4 = myApp.addView('#view-4', {
+    // 让这个视图支持动态导航栏
+    dynamicNavbar: true
+});
+var view4 = myApp.addView('#view-5', {
+    // 让这个视图支持动态导航栏
+    dynamicNavbar: true
+});
+// 下面代码是给“关于”页面使用的（关于页面加载完毕后触发）
+
+// 方式1：通过页面回调 (推荐):
+myApp.onPageInit('type', function(page) {
+
+        var category = page.query.type;
+        var server = "https://topstore.vip/GetBrief/AppListByCategory.action?category="
+        $.ajax({
+            type: 'GET',
+            url: server + category,
+            async: true,
+            dataType: 'jsonp',
+            jsonp: "jsonpCallback",
+            success: function(data) {
+                var str = ""
+                for (var x = 0; x < data.AppList.length; x++) {
+                    str += addRow(data.AppList[x].id, data.AppList[x].name, data.AppList[x].developer, data.AppList[x].subtitle);
+
+                }
+                document.getElementById("title").innerHTML = category
+                document.getElementById("header").innerHTML = category
+                document.getElementById("appsByType").innerHTML = str;
+            },
+            error: function() {
+                alert("network error");
+            }
+        });
+    })
+    /*
+    // 方式2：通过pageInit事件处理所有页面
+    $$(document).on('pageInit', function (e) {
+        // 获取页面数据
+        var page = e.detail.page;
+
+        //判断是否是“关于”页面
+        if (page.name === 'type') {
+            myApp.alert('"关于"页面加载完毕2!');
+        }
+    })
+
+    */
+$$(document).on('pageInit', '.page[data-page="index-1"]', function(e) {
+    init(appData);
+})
+$$(document).on('pageInit', '.page[data-page="index-2"]', function(e) {
+    init(appData);
+})
+$$(document).on('pageInit', '.page[data-page="index-3"]', function(e) {
+    init(appData);
+})
+$$(document).on('pageInit', '.page[data-page="index-4"]', function(e) {
+    init(appData);
+})
+
+function addRow(id, name, developer, subtitle) {
+    var str = "                                    <li class=\"item-content\">\n" +
+        "                                        <div class=\"item-media\">\n" +
+        "                                            <img src=\"https://topstore.vip/image/" + id + ".png\" width=\"44\">\n" +
+        "                                        </div>\n" +
+        "                                        <div class=\"item-inner\">\n" +
+        "                                            <div class=\"item-title-row\">\n" +
+        "                                                <div class=\"app-title\">" + reserve(name) + "</div>\n" +
+        "                                                <div class=\"\" style=\"margin-right: 20px;\">\n" +
+        "                                                    <a href=\"detail.html?id=" + id + "\" class=\"button button-fill button-round\"style=\"background: #F0F1F6; color: #007AFF; font-weight:bold;\">GET</a>\n" +
+        "                                                </div>\n" +
+        "                                            </div>\n" +
+        "                                            <div class=\"app-subtitle\">" + reserve(subtitle) + "</div>\n" +
+        "                                        </div>\n" +
+        "                                    </li>"
+    return str;
+}
+myApp.onPageInit('detail', function(page) {
+    var id = page.query.id;
+    var server = "https://topstore.vip/GetDetail/AppDetail.action?id="
+    $.ajax({
+        type: 'GET',
+        url: server + id,
+        async: true,
+        dataType: 'jsonp',
+        jsonp: "jsonpCallback",
+        success: function(data) {
+            addDetail(data);
+        },
+        error: function() {
+            alert("network error");
+        }
+    });
+    var id = page.query.id;
+    var server = "https://topstore.vip/GetCategory/AppCategory.action?id="
+    $.ajax({
+        type: 'GET',
+        url: server + id,
+        async: true,
+        dataType: 'jsonp',
+        jsonp: "jsonpCallback",
+        success: function(data) {
+            appCategory(data);
+        },
+        error: function() {
+            alert("network error");
+        }
+    });
+})
+
+function addDetail(data) {
+    document.getElementById("avatar").src = "https://topstore.vip/image/" + data.id + ".png"
+    document.getElementById("preview").src = "https://topstore.vip/preview/" + data.id + ".png"
+    document.getElementById("name").innerHTML = data.name
+    document.getElementById("subtitle").innerHTML = data.subtitle
+    document.getElementById("downloadLink").href = "itms-services://?action=download-manifest&url=https://topstore.vip/plist/" + data.id + ".plist"
+    document.getElementById("Size").innerHTML = data.filesize
+    var predata = data.description.substring(0, 20);
+    var postdata = data.description.substr(20);
+    var description = predata + "<div class=\"desc\" style=\"display: none;\">" + postdata + "</div>" +
+        "<p>\n" +
+        "  <a href=\"#\" onclick=\"if ($$('.desc').css('display') == 'none'){$$('.desc').css('display', 'inline'); this.innerHTML='Less...'} else {$$('.desc').css('display', 'none'); this.innerHTML='More...'}\" class=\"\">More...</a>\n" +
+        "</p>";
+    document.getElementById("Describtion").innerHTML = description.replace(/\n/g, "<br>")
+
+    document.getElementById("Developer").innerHTML = data.developer
+    document.getElementById("Version").innerHTML = data.version
+}
+
+function appCategory(data) {
+    for (var x = 0; x < data.category.length; x++) {
+        $("#Category").append(getCategory(data.category[x].categoryId) + "<br/>");
+    }
+}
+myApp.onPageInit('index-4', function(page) {
+    alert("search")
+})
+
+function getCategory(id) {
+    if (id == "1")
+        return "Tweaked Apps"
+    else if (id == "2")
+        return "Paid and Other Apps"
+    else if (id == "3")
+        return "Tweaked Games"
+    else if (id == "4")
+        return "Paid and Other Games"
+    else if (id == "27")
+        return "Recommended This Week"
+    else if (id == "31")
+        return "Movies on iOS"
+    else if (id == "32")
+        return "Top Apps"
+    else
+        return ""
+}
