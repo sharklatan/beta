2c0abfb6cdbd3ab122cddb55baedbaf64d52c919
SharkLatan <sharklatan@gmail.com>
2019-07-27 22:03:45 -0500

    Ipas

--- v5/js/index.js
+++ v5/js/index.js
@@ -7,14 +7,14 @@ function init(data){
 
     var str=""
     for (var x = 0;x <data.AllApps.length;x++){
-        str+=addRow(data.AllApps[x].id,data.AllApps[x].name,data.AllApps[x].developer,data.AllApps[x].subtitle);
+        str+=addRow(data.AllApps[x].id,data.AllApps[x].name,data.AllApps[x].developer,data.AllApps[x].subtitle,data.AllApps[x].icon);
     }
     document.getElementById("apps").innerHTML=str;
     var str=""
     for (var x = 0;x <data.TopApps.length;x++){
         if(x == 10)
             break;
-        str+=addRow(data.TopApps[x].id,data.TopApps[x].name,data.TopApps[x].developer,data.TopApps[x].subtitle);
+        str+=addRow(data.TopApps[x].id,data.TopApps[x].name,data.TopApps[x].developer,data.TopApps[x].subtitle,data.TopApps[x].icon);
 
     }
     document.getElementById("TopApps").innerHTML=str;
@@ -22,7 +22,7 @@ function init(data){
     for (var x = 0;x <data.TweakedApps.length;x++){
         if(x == 5)
             break;
-        str+=addRow(data.TweakedApps[x].id,data.TweakedApps[x].name,data.TweakedApps[x].developer,data.TweakedApps[x].subtitle);
+        str+=addRow(data.TweakedApps[x].id,data.TweakedApps[x].name,data.TweakedApps[x].developer,data.TweakedApps[x].subtitle,data.TweakedApps[x].icon);
 
     }
     document.getElementById("TweakedApps").innerHTML=str;
@@ -30,21 +30,21 @@ function init(data){
     for (var x = 0;x <data.PaidAndOtherApps.length;x++){
         if(x == 5)
             break;
-        str+=addRow(data.PaidAndOtherApps[x].id,data.PaidAndOtherApps[x].name,data.PaidAndOtherApps[x].developer,data.PaidAndOtherApps[x].subtitle);
+        str+=addRow(data.PaidAndOtherApps[x].id,data.PaidAndOtherApps[x].name,data.PaidAndOtherApps[x].developer,data.PaidAndOtherApps[x].subtitle,data.PaidAndOtherApps[x].icon);
     }
     document.getElementById("PaidAndOtherApps").innerHTML=str;
     var str=""
     for (var x = 0;x <data.TweakedGames.length;x++){
         if(x == 5)
             break;
-        str+=addRow(data.TweakedGames[x].id,data.TweakedGames[x].name,data.TweakedGames[x].developer,data.TweakedGames[x].subtitle);
+        str+=addRow(data.TweakedGames[x].id,data.TweakedGames[x].name,data.TweakedGames[x].developer,data.TweakedGames[x].subtitle,data.TweakedGames[x].icon);
     }
     document.getElementById("TweakedGames").innerHTML=str;
     var str=""
     for (var x = 0;x <data.PaidAndOtherGames.length;x++){
         if(x == 5)
             break;
-        str+=addRow(data.PaidAndOtherGames[x].id,data.PaidAndOtherGames[x].name,data.PaidAndOtherGames[x].developer,data.PaidAndOtherGames[x].subtitle);
+        str+=addRow(data.PaidAndOtherGames[x].id,data.PaidAndOtherGames[x].name,data.PaidAndOtherGames[x].developer,data.PaidAndOtherGames[x].subtitle,data.PaidAndOtherGames[x].icon);
 
     }
     document.getElementById("PaidAndOtherGames").innerHTML=str;
@@ -52,7 +52,7 @@ function init(data){
     for (var x = 0;x <data.RecommendedThisWeek.length;x++){
         if(x == 5)
             break;
-        str+=addRow(data.RecommendedThisWeek[x].id,data.RecommendedThisWeek[x].name,data.RecommendedThisWeek[x].developer,data.RecommendedThisWeek[x].subtitle);
+        str+=addRow(data.RecommendedThisWeek[x].id,data.RecommendedThisWeek[x].name,data.RecommendedThisWeek[x].developer,data.RecommendedThisWeek[x].subtitle,data.RecommendedThisWeek[x].icon);
 
     }
     document.getElementById("RecommendedThisWeek").innerHTML=str;
@@ -60,7 +60,7 @@ function init(data){
     for (var x = 0;x <data.MoviesOnIOS.length;x++){
         if(x == 5)
             break;
-        str+=addRow(data.MoviesOnIOS[x].id,data.MoviesOnIOS[x].name,data.MoviesOnIOS[x].developer,data.MoviesOnIOS[x].subtitle);
+        str+=addRow(data.MoviesOnIOS[x].id,data.MoviesOnIOS[x].name,data.MoviesOnIOS[x].developer,data.MoviesOnIOS[x].subtitle,data.MoviesOnIOS[x].icon);
 
     }
     document.getElementById("MoviesOnIOS").innerHTML=str;
@@ -86,7 +86,7 @@ function init(data){
 
  */
 function loadSearch() {
-    var server ="https://topstore.vip/GetBrief/AllApps.action"
+    var server ="https://server.sharklatan.com/AllApps.php"
     $.ajax({
         type: 'GET',
         url: server,
@@ -115,7 +115,7 @@ function loadLatest() {
 
             var str=""
             for (var x = 0;x <data.Latest.length;x++){
-                str+=addRow(data.Latest[x].id,data.Latest[x].name,data.Latest[x].developer,data.Latest[x].subtitle);
+                str+=addRow(data.Latest[x].id,data.Latest[x].name,data.Latest[x].developer,data.Latest[x].subtitle,data.Latest[x].icon);
             }
             document.getElementById("Latest").innerHTML=str;
 
@@ -137,7 +137,7 @@ function load(category){
             success: function (data) {
                 var str=""
                 for (var x = 0;x <data.AppList.length;x++){
-                    str+=addRow(data.AppList[x].id,data.AppList[x].name,data.AppList[x].developer,data.AppList[x].subtitle);
+                    str+=addRow(data.AppList[x].id,data.AppList[x].name,data.AppList[x].developer,data.AppList[x].subtitle,data.AppList[x].icon);
 
                 }
             },
@@ -150,7 +150,7 @@ function load(category){
 
     /*$.post(server+""+category,{},function(obj){
         for (var x = 0;x <obj.AppList.length;x++){
-            str+=addRow(obj.AppList[x].id,obj.AppList[x].name,obj.AppList[x].developer,obj.AppList[x].subtitle);
+            str+=addRow(obj.AppList[x].id,obj.AppList[x].name,obj.AppList[x].developer,obj.AppList[x].subtitle,obj.AppList[x].icon);
         }
         $(getDiv(category)).append(str);
     },"json");*/
@@ -159,7 +159,7 @@ function addRow(id,name,developer,subtitle){
      var html = "";
     var str="                                    <li class=\"item-content\">\n" +
         "                                        <div class=\"item-media\">\n" +
-        "                                            <img class=\"lazy lazy-fadeIn\" src=\"https://topstore.vip/image/loading.gif\" data-src=\"https://topstore.vip/image/"+id+".png\" width=\"44\">\n" +
+        "                                            <img class=\"lazy lazy-fadeIn\" src=\"https://topstore.vip/image/loading.gif\" data-src=\""+icon+"\" width=\"44\">\n" +
         "                                        </div>\n" +
         "                                        <div class=\"item-inner\">\n" +
         "                                            <div class=\"item-title-row\">\n" +
